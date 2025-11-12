608f9b71a02fb0bbc1b704fe51c07126d880e978
Cheva <super_chen@126.com>
2025-11-12 20:51:46 +0800

    feat:测试YouTube视频字幕翻译功能，测试用cookies下载

--- download_subtitles.py
+++ download_subtitles.py
@@ -12,6 +12,7 @@ def download_subtitles(url):
         'writeautomaticsub': True,       # 下载自动生成的字幕
         'skip_download': True,           # 跳过视频下载
         'subtitleslangs': ['en'],       # 下载英文字幕
+        'cookies':'cookies.txt'
         'quiet': True,                   # 减少控制台输出
         'outtmpl': 'subtitles/%(title)s.%(ext)s'  # 字幕输出路径模板
     }
